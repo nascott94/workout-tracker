5e9db3f8ec597a73330db183f5e8392c2af432a9
nascott <nascott94@outlook.com>
2021-09-22 19:44:36 -0600

    finished routes

--- routes/api.js
+++ routes/api.js
@@ -35,3 +35,23 @@ module.exports = function (app) {
       });
   });
 
+  app.post("/api/workouts/range", function (req, res) {
+    Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
